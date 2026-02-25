7a22f80d3f72e06233a7d1b5c81d711f6fa71c2f
Christopher Flores <Christopher321flores@gmail.com>
2026-02-25 16:23:58 -0500

    adding a footer and need to fix when it switches to mobile

---
+++ src/pages/components/footer.jsx
@@ -0,0 +1,19 @@
+import '../../CSS/footer.css'
+function Footer(){
+
+    return (
+        <>
+            <div className="footer_body">
+                <div className="content">
+                    <p>Ballet Folklorico de Puebla York</p>
+                    <p>____</p>
+                    <p>@ 2026 All rights reserved</p>
+                    <p>Balletfolklroicopueblayork@gmail.com</p>
+                    <p>917-562-9571</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,13 @@
 import NavBar from './components/navBar';
+import Footer from './components/footer';
 function Home(){
     return (
-        <>
+        <div className='pages'>
+            
             <NavBar />
-        </>
+
+            <Footer/>
+        </div>  
     );
 }
 
