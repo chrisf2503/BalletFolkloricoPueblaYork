4f961ae430e59bbdcaf0a4a5f25e37f3328866ed
Christopher Flores <Christopher321flores@gmail.com>
2026-02-25 09:34:53 -0500

    adding functional nav that collapes onces it reaches at a certain length

--- src/pages/components/navBar.jsx
+++ src/pages/components/navBar.jsx
@@ -1,9 +1,10 @@
 import '../../CSS/navBar.css'
 function navBar(){
+    
     return (
         <>
             <div className='navContainer'>
-                <div className='title'><h3>Ballet Folklorico de Puebla York</h3></div>
+                <div className='title'><p className='name'>Ballet Folklorico de Puebla York</p></div>
                 <div className="linkContainer">
                     <div className="links"><p>Gallary</p></div>
                     <div className="links"><p>About Us</p></div>
